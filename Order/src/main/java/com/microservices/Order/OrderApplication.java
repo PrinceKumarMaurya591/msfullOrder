27a76d44e8542ae3837c8002f247ce62dbb7e7ba
PrinceKumarMaurya591 <72147360+PrinceKumarMaurya591@users.noreply.github.com>
2023-12-05 02:14:17 +0530

    first commit without test

--- Order/src/main/java/com/microservices/Order/OrderApplication.java
+++ Order/src/main/java/com/microservices/Order/OrderApplication.java
@@ -2,10 +2,12 @@ package com.microservices.Order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+
 public class OrderApplication {
 
 	public static void main(String[] args) {
